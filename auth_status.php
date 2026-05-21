8015e70711dd4d4880e4c8549b2a0208ec5690d1
JHao0313 <ngujiahao@gmail.com>
2026-05-21 23:24:08 +0800

    Delete auth_status.php

--- auth_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'auth.php';
-header('Content-Type: application/json');
-echo json_encode([
-    'logged_in' => !empty($_SESSION['email']) && current_user() !== null,
-    'role' => (int)($_SESSION['role'] ?? 0)
-]);
-exit();
-?>
